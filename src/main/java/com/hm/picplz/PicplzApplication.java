b8145d5e4474079ab956016525f3607993f9422f
chaeshee0908 <chaeshee0908@naver.com>
2024-07-30 19:41:16 +0900

    feat: spring 기본 세팅 #1

---
+++ src/main/java/com/hm/picplz/PicplzApplication.java
@@ -0,0 +1,13 @@
+package com.hm.picplz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicplzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicplzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hm/picplz/PicplzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.picplz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicplzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
