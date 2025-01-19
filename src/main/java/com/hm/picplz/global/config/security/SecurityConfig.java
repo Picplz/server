2cf8ef6ca2cdd135e2f9ab0c5ef2634b7c2939b8
psihyeong <qkrtlgud3286@naver.com>
2025-01-20 01:56:47 +0900

    feat: jwt 설정

--- src/main/java/com/hm/picplz/global/config/security/SecurityConfig.java
+++ src/main/java/com/hm/picplz/global/config/security/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.hm.picplz.global.config.security;
 
+import com.hm.picplz.domain.auth.jwt.JwtAccessDeniedHandler;
+import com.hm.picplz.domain.auth.jwt.JwtAuthenticationEntryPoint;
+import com.hm.picplz.domain.auth.jwt.JwtFilter;
+import com.hm.picplz.domain.auth.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,14 +16,20 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtTokenProvider jwtTokenProvider;
+
     /* 권한 제외 대상 */
     private static final String[] permitAllUrl = new String[]{
-        /** Swagger Docs*/"/api-docs/**", "/swagger-ui/**"
+            /** Swagger Docs*/"/api-docs/**", "/swagger-ui/**"
     };
     /* Admin 접근 권한 */
     private static final String[] permitAdminUrl = new String[]{
@@ -27,26 +38,33 @@ public class SecurityConfig {
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig)
-        throws Exception {
+            throws Exception {
         return authConfig.getAuthenticationManager();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-            .csrf(AbstractHttpConfigurer::disable)
-            .cors(cors -> cors.configurationSource(CorsConfig.corsConfigurationSource()))
-            .sessionManagement((sessionManagement) ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(request -> {
-                request.requestMatchers(permitAllUrl)
-                    .permitAll();
-                request.requestMatchers(permitAdminUrl)
-                    .hasRole("ADMIN");
-                request.anyRequest()
-                    .authenticated();
-            })
-            .build();
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(CorsConfig.corsConfigurationSource())
+                )
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(authenticationManager -> authenticationManager
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                .addFilterBefore(new JwtFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .authorizeHttpRequests(request -> {
+                    request.requestMatchers(permitAllUrl)
+                            .permitAll();
+                    request.requestMatchers(permitAdminUrl)
+                            .hasRole("ADMIN");
+                    request.anyRequest()
+                            .authenticated();
+                })
+                .build();
     }
 
     @Bean
