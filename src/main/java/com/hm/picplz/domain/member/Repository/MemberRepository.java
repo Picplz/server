bf6fbbeb35f2e4aaa1886a4652514ae45f631ab2
psihyeong <qkrtlgud3286@naver.com>
2025-01-22 14:47:24 +0900

    fix: 중복 MemberRepository 삭제

--- src/main/java/com/hm/picplz/domain/member/Repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hm.picplz.domain.member.Repository;
-
-import com.hm.picplz.domain.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
